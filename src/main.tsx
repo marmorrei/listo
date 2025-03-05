7a4c32c34b4f8a7bee97fa84969f15deed8cc355
Maria Morales Reina <124587546+marmorrei@users.noreply.github.com>
2025-03-05 13:44:32 +0100

    complete-styles-in-main-and-app-component

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,24 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
   body {
-  margin: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-width: 320px;
-  min-height: 100vh;
-  font-family: "Patrick Hand", serif;
-  font-weight: 400;
-  font-style: normal;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 320px;
+    min-height: 100vh;
+  }
+
+  * {
+    margin: 0;
+    font-family: "Patrick Hand", serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  #root {
+    display: flex;
+    width: 100%;
   }
 `
 
